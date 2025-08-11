545010297310a7419d548b5f2e96c38d58c0791d
MelidaZuniga55 <melidazuniga55@outlook.com>
2025-08-10 18:36:23 -0600

    Modificacion en ejercicio 1

--- POO/ejercicio1.js
+++ POO/ejercicio1.js
@@ -1,4 +1,3 @@
-// Clase para representar una cabecera de página web
 var CabeceraPagina = /** @class */ (function () {
     function CabeceraPagina(titulo, color, fuente) {
         this.titulo = titulo;
@@ -6,20 +5,29 @@ var CabeceraPagina = /** @class */ (function () {
         this.fuente = fuente;
         this.alineacion = "centro"; // Valor por defecto
     }
-    // Método para configurar cómo se alinea el título
+    // Primer método: obtener título, color y fuente
+    CabeceraPagina.prototype.obtenerDatos = function () {
+        return {
+            titulo: this.titulo,
+            color: this.color,
+            fuente: this.fuente,
+        };
+    };
+    // Segundo método: configurar alineación
     CabeceraPagina.prototype.configurarAlineacion = function (alineacion) {
         this.alineacion = alineacion;
     };
-    // Método que muestra todas las propiedades
+    // Tercer método: imprimir todas las propiedades
     CabeceraPagina.prototype.mostrarCabecera = function () {
-        console.log("Título:", this.titulo);
-        console.log("Color:", this.color);
-        console.log("Fuente:", this.fuente);
+        var datos = this.obtenerDatos();
+        console.log("Título:", datos.titulo);
+        console.log("Color:", datos.color);
+        console.log("Fuente:", datos.fuente);
         console.log("Alineación:", this.alineacion);
     };
     return CabeceraPagina;
 }());
 // Crear objeto y usar métodos
 var cabecera1 = new CabeceraPagina("Mi Página", "Negro", "Arial");
-cabecera1.configurarAlineacion("centro");
+cabecera1.configurarAlineacion("derecha");
 cabecera1.mostrarCabecera();
